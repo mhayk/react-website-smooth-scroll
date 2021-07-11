606a89de1d4d7378bd74fdfc42444aeeda9816a9
Mhayk Whandson <hi@mhayk.com>
2021-07-11 13:21:35 +0100

    Nav bar items have been added and styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,17 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElments";
+import { FaBars } from "react-icons/fa";
+
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElments";
 
 const Navbar = () => {
   return (
@@ -7,6 +19,26 @@ const Navbar = () => {
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+            <NavBtn>
+              <NavBtnLink to="/signin">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
     </>
