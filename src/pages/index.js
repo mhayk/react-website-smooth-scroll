9044d94027ca9aa08fce5e719cdafdf9be55cafc
Mhayk Whandson <hi@mhayk.com>
2021-07-12 21:28:48 +0100

    InfoSection componenet has been imported.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import {
+  homeObjOne,
+  homeObjThree,
+  homeObjTwo,
+} from "../components/InfoSection/Data";
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,6 +22,9 @@ function Home() {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
     </>
   );
 }
