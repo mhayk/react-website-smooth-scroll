c5fb8140a83a281d2d280be6c4946551c4e2c8e2
Mhayk Whandson <hi@mhayk.com>
2021-07-14 23:20:59 +0100

    Transparence background is removed when the scrolls is more than 80px.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 
 import {
@@ -14,9 +14,23 @@ import {
 } from "./NavbarElments";
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
   return (
     <>
-      <Nav>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
           <NavLogo to="/">dolla</NavLogo>
           <MobileIcon onClick={toggle}>
