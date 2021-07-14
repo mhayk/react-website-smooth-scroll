acf85b9b3b32ec759a77bc3b0ed5ad040058ded2
Mhayk Whandson <hi@mhayk.com>
2021-07-14 23:15:25 +0100

    Footer component has been created and styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import {
   FooterContainer,
   FooterLink,
   FooterLinkItems,
@@ -7,6 +14,12 @@ import {
   FooterLinksWrapper,
   FooterLinkTitle,
   FooterWrap,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+  SocialMedia,
+  SocialMediaWrap,
+  WebsiteRights,
 } from "./FooterElements";
 
 function Footer() {
@@ -47,6 +60,31 @@ function Footer() {
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">dolla</SocialLogo>
+            <WebsiteRights>
+              dolla © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" arial-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" arial-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" arial-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" arial-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" arial-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   );
